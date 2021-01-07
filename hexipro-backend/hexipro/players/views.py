f99afdec09f57a04fa1168f65eb7b453e64ed72b
Ethan Uzarowski <ethan.uzarowski@ukg.com>
2021-01-07 17:06:35 -0500

    Starting socket stuff

---
+++ hexipro-backend/hexipro/players/views.py
@@ -0,0 +1,7 @@
+from .models import Player, Board, Tile
+
+def allocate(player_name, col, row):
+    if Tile.objects.get(row=row, column=col).owner==Player.objects.get(name=player_name):
+        Tile.objects.get(row=row, column=col).power += 1
+        Tile.objects.get(row=row, column=col).save()
+        
